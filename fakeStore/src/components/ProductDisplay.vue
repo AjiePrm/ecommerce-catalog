1cf3774dad9197354058bf53630bcedf2c516923
AJIE SALAHUDIN PRIMA <ajiee.salahudin@gmail.com>
2023-02-28 00:35:37 +0700

    slicing ui progress

--- fakeStore/src/components/ProductDisplay.vue
+++ fakeStore/src/components/ProductDisplay.vue
@@ -4,7 +4,14 @@
 
 <template>
     <div class="container-wrapper">
-       hello word !
+       <div class="img-cover">
+        <img class="img-content" src="https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg" alt="">
+       </div>
+
+       <div class="container-detail">
+        <h1 class="title-female">DANVOUY Womens T Shirt Casual Cotton Short</h1>
+       </div>
+
     </div>
 </template>
 
