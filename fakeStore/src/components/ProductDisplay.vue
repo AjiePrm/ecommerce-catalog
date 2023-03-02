3fcbff2f7e9999a1271f70b385b395050b26219f
AJIE SALAHUDIN PRIMA <ajiee.salahudin@gmail.com>
2023-03-02 08:53:40 +0700

    slicing ui update

--- fakeStore/src/components/ProductDisplay.vue
+++ fakeStore/src/components/ProductDisplay.vue
@@ -10,7 +10,25 @@
 
        <div class="container-detail">
         <h1 class="title-female">DANVOUY Womens T Shirt Casual Cotton Short</h1>
+        
+        <div class="categorise">
+        <h5 class="item-tag">Women's clothing</h5>
        </div>
+
+       <div class="description">
+        <p class="decript-tag">100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER), Faux leather material for style and comfort / 2 pockets of front, 2-For-One Hooded denim style faux leather jacket, Button detail on waist / Detail stitching at sides, HAND WASH ONLY / DO NOT BLEACH / LINE DRY / DO NOT IRON </p>
+       </div>
+
+       <div class="bottom-detail">
+        <h1 class="title-female"> $22.28</h1>
+       </div>
+
+       <div class="button-styel">
+        <button class="btn-buy" type="submit">Buy Now</button>
+        <button class="btn-next" type="submit">Next Product</button>
+       </div>
+       </div>
+       
 
     </div>
 </template>
