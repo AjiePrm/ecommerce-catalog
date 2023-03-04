25178d8933e5e1e2428cb3c3075731b1a6db7f12
AJIE SALAHUDIN PRIMA <ajiee.salahudin@gmail.com>
2023-03-04 23:14:23 +0700

    finishing fetch handler

--- fakeStore/src/components/ProductDisplay.vue
+++ fakeStore/src/components/ProductDisplay.vue
@@ -1,49 +1,69 @@
 <script>
 
 export default {
-    data() {
-      return {
-        data: null,
-      };
-    },
-    methods: {
-      async fetchData() {      
-        const response = await fetch("https://fakestoreapi.com/products/");      
-        this.data = await response.json();
-      },    
-    },
-    mounted() {
-      this.fetchData();
-    },
-  };
+  data() {
+    return {
+      imgLoading: null,
+      allData: null,
+      data: null,
+      index: 1,
+    };
+  },
+  methods: {
+    async fetchData() {  
+      const responseAllData = await fetch ("https://fakestoreapi.com/products/")    
+      const response = await fetch(`https://fakestoreapi.com/products/${this.index}`);      
+      this.data = await response.json();
+      this.allData = await responseAllData.json()
+      this.imgLoading = this.data.image
+    },  
 
+    onClickHandler() {
+      this.data = null
+      this.imgLoading = null
+      if (this.index >= this.allData.length) {
+        this.index = 1;
+      } else {
+        this.index++;
+      }
+      this.fetchData()
+    },
+  },
+  mounted() {
+    this.fetchData();
+  },
+  watch: {
+    index(newValue, oldValue) {
+      console.log(`index changed from ${oldValue} to ${newValue}`);
+    }
+  }
+};
 </script>
 
-
-
 <template>
-    <div class="container-wrapper">
+    <div v-if="!data && !imgLoading">Loading...</div>
+    <div v-else class="container-wrapper">
        <div class="img-cover">
-        <img v-if="data" class="img-content" :src="data[0].image" alt="Product image">
+        <img v-if="data" class="img-content" :src="data.image" alt="Product image">
        </div>
        <div class="container-detail">
-        <div v-if="!data">Loading</div>
-        <h1 v-else class="title-female">{{ data[0].title }}</h1>
+        <h1 v-if="data" class="title-female">{{ data.title }}</h1>
         <div class="categorise">
-        <h5 v-if="data" class="item-tag">{{ data[0].category }}</h5>
+        <h5 v-if="data" class="item-tag">{{ data.category }}</h5>
+        <h5 v-if="data" class="item-rate">{{data.rating.rate}}</h5>
        </div>
 
        <div class="description">
-        <p v-if="data" class="decript-tag">{{ data[0].description }}</p>
+        <p v-if="data" class="decript-tag">{{ data.description }}</p>
        </div>
 
        <div class="bottom-detail">
-        <h1 v-if="data" class="title-female">${{data[0].price}}</h1>
+        <h1 v-if="data" class="title-female">$ {{data.price}}</h1>
        </div>
 
        <div class="button-styel">
         <button class="btn-buy" type="submit">Buy Now</button>
-        <button class="btn-next" type="submit" >Next Product</button>
+        <button class="btn-next" type="submit" @click="onClickHandler();" >Next Product</button>
        </div>
        </div>
        
